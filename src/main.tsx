98601088b12830fc2b1eb53ca63eeb1c9ca89af1
DaVid Justin <chaosdudeserad@gmail.com>
2025-09-08 19:44:07 +0800

    refaractor the main page to have a layout

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './App.css'
 import Home from './pages/home.tsx'
+import About from './pages/about.tsx'
+import Project from './pages/project.tsx'
+import Contact from './pages/contact.tsx'
+import PageLayout from './components/layouts/page-layout.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Home/>
+    <PageLayout>
+      <Home/>
+      <About/>
+      <Project/>
+      <Contact/>
+    </PageLayout>
   </StrictMode>,
 )
